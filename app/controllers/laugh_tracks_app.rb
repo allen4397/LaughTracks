59f3e92ee1bfda2ba8fb7e02629ef3991ccf3bcb
Tim Allen <tnelsallen3@gmail.com>
2019-01-25 09:12:31 -0700

    Adds comedians info to index page

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,5 +1,6 @@
 class LaughTracksApp < Sinatra::Base
   get '/comedians' do
+    @comedians = Comedian.all
     erb :"comedians/index"
   end
 end
--- spec/features/comedians_index_spec.rb
+++ spec/features/comedians_index_spec.rb
@@ -1,11 +1,20 @@
 RSpec.describe "when I visit comedians index page" do
-  it 'shows me a list of all comedians' do
+  it 'shows me a list of all comedians names, ages and cities' do
     mulaney = Comedian.create(name: "John Mulaney", age: 34, city: "Chicago")
     seinfeld = Comedian.create(name: "Jerry Seinfeld", age: 62, city: "Massapequa")
 
     visit '/comedians'
 
-    expect(page).to have_content("John Mulaney")
-    expect(page).to have_content("Jerry Seinfeld")
+    within '#comedian-1' do
+      expect(page).to have_content("John Mulaney")
+      expect(page).to have_content(34)
+      expect(page).to have_content("Chicago")
+    end
+
+    within '#comedian-2' do
+      expect(page).to have_content("Jerry Seinfeld")
+      expect(page).to have_content(62)
+      expect(page).to have_content("Massapequa")
+    end
   end
 end
