d87fcd825426a1aeb5d851ce7e651956a779d75e
Tim Allen <tnelsallen3@gmail.com>
2019-01-25 11:26:58 -0700

    Adds run_time column to db

--- spec/features/comedians_index_spec.rb
+++ spec/features/comedians_index_spec.rb
@@ -36,4 +36,23 @@ RSpec.describe "when I visit comedians index page" do
       expect(page).to have_content("Jerry Before Seinfeld")
     end
   end
+
+  context 'Where I see a list of each comedians TV specials names' do
+    it 'shows each specials run-time length in minutes, and a thumbnail image' do
+      mulaney = Comedian.create(name: "John Mulaney", age: 34, city: "Chicago")
+      seinfeld = Comedian.create(name: "Jerry Seinfeld", age: 62, city: "Massapequa")
+      new_in_town = Special.create(name: "New In Town", comedian_id: 1, run_time: 80)
+      jerry_before_seinfeld = Special.create(name: "Jerry Before Seinfeld", comedian_id: 2, run_time: 80)
+
+      visit '/comedians'
+
+      within '#comedian-1' do
+        expect(page).to have_content(80)
+      end
+
+      within '#comedian-2' do
+        expect(page).to have_content(80)
+      end
+    end
+  end
 end
