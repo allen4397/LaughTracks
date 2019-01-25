0dcd94dba0f5f9fede4e70c91ea6a3e6f0c55753
Tim Allen <tnelsallen3@gmail.com>
2019-01-25 10:23:57 -0700

    Adds tv specials to index page

--- spec/features/comedians_index_spec.rb
+++ spec/features/comedians_index_spec.rb
@@ -21,6 +21,19 @@ RSpec.describe "when I visit comedians index page" do
   it "shows me each comedians tv specials" do
     mulaney = Comedian.create(name: "John Mulaney", age: 34, city: "Chicago")
     seinfeld = Comedian.create(name: "Jerry Seinfeld", age: 62, city: "Massapequa")
-    new_in_town = Special.create(name: "New In Town")
+    new_in_town = Special.create(name: "New In Town", comedian_id: 1)
+    comeback_kid = Special.create(name: "The Comeback Kid", comedian_id: 1)
+    jerry_before_seinfeld = Special.create(name: "Jerry Before Seinfeld", comedian_id: 2)
 
+    visit '/comedians'
+
+    within '#comedian-1' do
+      expect(page).to have_content("New In Town")
+      expect(page).to have_content("The Comeback Kid")
+    end
+
+    within '#comedian-2' do
+      expect(page).to have_content("Jerry Before Seinfeld")
+    end
+  end
 end
