c9b7fabae456bd63c1b59eafb3dc6f2d119e229f
Tim Allen <tnelsallen3@gmail.com>
2019-01-25 12:55:08 -0700

    Adds image url column to special db

--- spec/features/comedians_index_spec.rb
+++ spec/features/comedians_index_spec.rb
@@ -54,5 +54,24 @@ RSpec.describe "when I visit comedians index page" do
         expect(page).to have_content(80)
       end
     end
+
+    it 'shows a thumbnail image for each special' do
+      mulaney = Comedian.create(name: "John Mulaney", age: 34, city: "Chicago")
+      seinfeld = Comedian.create(name: "Jerry Seinfeld", age: 62, city: "Massapequa")
+      url_1 = "https://m.media-amazon.com/images/M/MV5BOTc2N2I0Y2UtOTgyYy00MTU3LTk1YzItZGIwMDcxY2JkMGQzXkEyXkFqcGdeQXVyMjQzNzk2ODk@._V1_UY268_CR1,0,182,268_AL_.jpg"
+      url_2 = "https://m.media-amazon.com/images/M/MV5BMjIzNzg5NTA2M15BMl5BanBnXkFtZTgwOTY4NjA2MDE@._V1_UY1200_CR135,0,630,1200_AL_.jpg"
+      new_in_town = Special.create(name: "New In Town", comedian_id: 1, run_time: 80, image_url: url_1)
+      jerry_before_seinfeld = Special.create(name: "Jerry Before Seinfeld", comedian_id: 2, run_time: 80, image_url: url_2)
+
+      visit '/comedians'
+
+      within '#comedian-1' do
+        expect(page).to have_css("img[src*='#{url_1}']")
+      end
+
+      within '#comedian-2' do
+        expect(page).to have_css("img[src*='#{url_2}']")
+      end
+    end
   end
 end
